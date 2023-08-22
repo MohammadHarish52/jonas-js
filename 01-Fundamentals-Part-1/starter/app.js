215a49b52f03a886cb582ae3af61afbfa0d824e0
tachyonok <tachyonparticle123@gmail.com>
2023-08-22 18:55:30 +0530

    jonas js course

---
+++ 01-Fundamentals-Part-1/starter/app.js
@@ -0,0 +1,13 @@
+let js = "amazing";
+console.log(4+5-1);
+
+
+let fname = "PD";
+
+//camel case 
+//firstNamePerson
+//first_name
+
+// cannot write 3years while naming variables
+//String
+console.log(fname);
