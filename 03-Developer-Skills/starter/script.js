30e2d8c2d9d333ce5e1a8db781ba549774db20f1
tachyonok <tachyonparticle123@gmail.com>
2023-09-04 14:09:01 +0530

    manipulating dom

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,8 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const x = 24;
+if (x === 23) console.log(23);
+else console.log("fuck u");
+
+//BUG
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+// Dom manipulation
+let message = (document.querySelector('.message').innerHTML = `Correct Number`);
+console.log(message);
+
+// what is DOM
+// DOM is not a part of js instead it is a part of WEb APIS
+// Dom manipulation
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23;
