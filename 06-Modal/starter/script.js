b62a2a7a264d6db3d09f40fb1d04ba48020f2f53
tachyonok <tachyonparticle123@gmail.com>
2023-09-05 21:15:24 +0530

    Modal component completed

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,28 @@
-'use strict';
+const modalButtons = document.querySelectorAll('.show-modal');
+const modalComponent = document.querySelector('.modal');
+const closeButton = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+const openModal = () => {
+  modalComponent.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+  modalComponent.focus();
+};
+
+modalButtons.forEach(button => {
+  button.addEventListener('click', openModal);
+});
+
+const closeModal = () => {
+  modalComponent.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+closeButton.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', event => {
+  if (event.key === 'Escape' && !modalComponent.classList.contains('hidden')) {
+    closeModal();
+    console.log('key was pressed');
+  }
+});
