7f35cabcb941fd37d1cde9ac30d60ac6b9bcddbd
tachyonok <tachyonparticle123@gmail.com>
2023-09-07 23:15:20 +0530

    hold

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -21,30 +21,59 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = () => {
+  //switch to next player
+  player0.classList.toggle('player--active');
+  player1.classList.toggle('player--active');
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent = 0; // setting the current score to 0
+  activePlayer = activePlayer === 0 ? 1 : 0;
+};
 
 btnRoll.addEventListener('click', () => {
   // generating a random dice roll
-  const randomNumber = Math.floor(Math.random() * 6) + 1;
-  // diplay dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${randomNumber}.png`;
-  //check for rolled 1: true switch to next player
-  if (randomNumber !== 1) {
-    currentScore = currentScore + randomNumber;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-    // player1.classList.remove('player--active');
-    // player0.classList.add('player--active');
-  } else {
-    //switch player
-    currentScore = 0;
-    // player0.classList.remove('player--active');
-    // player1.classList.add('player--active');
-    //using toggle hoga to add krdega nhi hga to remve krdega
-    player0.classList.toggle('player--active');
-    player1.classList.toggle('player--active');
+  if (playing) {
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+    // diplay dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${randomNumber}.png`;
+    //check for rolled 1: true switch to next player
+    if (randomNumber !== 1) {
+      currentScore = currentScore + randomNumber;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+      // player1.classList.remove('player--active');
+      // player0.classList.add('player--active');
+    } else {
+      //switch player
+      switchPlayer();
+      // player0.classList.remove('player--active');
+      // player1.classList.add('player--active');
+      //using toggle hoga to add krdega nhi hga to remve krdega
+    }
+  }
+});
 
-    document.getElementById(`current--${activePlayer}`).textContent = 0; // setting the current score to 0
-    activePlayer = activePlayer === 0 ? 1 : 0;
+btnHold.addEventListener('click', () => {
+  if (playing) {
+    // Add current score to active players score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    //finish
+    if (scores[activePlayer] >= 100) {
+      diceEl.classList.add('hidden');
+      playing = false; //to stop game when someone wins
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
   }
 });
