86d7b0e95aee2f64da10f34dff8b3cc1d24bcfb2
tachyonok <tachyonparticle123@gmail.com>
2023-09-08 22:43:14 +0530

    scope chain

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+//Scoping in practise
+
+function calcAge(birthYear) {
+  const y = new Date();
+  const currentY = y.getUTCFullYear();
+  const age = currentY - birthYear;
+  function printAge() {
+    const output = `You are ${age} ,born in ${birthYear}`;
+    console.log(output);
+
+    // block scope
+    if (birthYear >= 1981 && birthYear <= 2003) {
+      const str = `Oh , and you're a milleneal , ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+  }
+  printAge();
+
+  // printage is lexically scoped with calcAge which is lexically scoped with global scope
+
+  return age;
+  // calcage will have access to global scope variables
+  // since the outer scope variables are accesible
+  // variable lookup
+}
+
+const firstName = 'Harish';
+calcAge(2002);
