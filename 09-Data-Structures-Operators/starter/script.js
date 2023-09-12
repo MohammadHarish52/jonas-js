3863dbe09a96a1025960573e6aa70789e703fa1b
tachyonok <tachyonparticle123@gmail.com>
2023-09-12 20:57:47 +0530

    challenge 1 football team

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -155,34 +155,37 @@ restaurant.orderDelievery({
 // const guestCorrect = restaurant.numGuests ?? 10;
 // console.log(guestCorrect); // 25
 
+////////////////////////////
 // logical assigment operators
 // Or assigment operator
 
-const rest1 = {
-  name: 'capri',
-  numOfguests: 20,
-};
+// const rest1 = {
+//   name: 'capri',
+//   numOfguests: 20,
+// };
 
-const rest2 = {
-  name: 'pizzeria',
-  owner: 'Zeff',
-};
+// const rest2 = {
+//   name: 'pizzeria',
+//   owner: 'Zeff',
+// };
 
-// rest1.numOfguests ||= 10;
-// rest2.numOfguests ||= 10;
+// // rest1.numOfguests ||= 10;
+// // rest2.numOfguests ||= 10;
 
-//Nullish assigment operator
-rest1.numOfguests ??= 10;
-rest2.numOfguests ??= 10;
+// //Nullish assigment operator
+// rest1.numOfguests ??= 10;
+// rest2.numOfguests ??= 10;
 
-console.log(rest1);
-console.log(rest2);
+// console.log(rest1);
+// console.log(rest2);
 
-//logical AND assignment operator
+// //logical AND assignment operator
 
-rest2.owner &&= 'anonymus';
+// rest2.owner &&= 'anonymus';
 
-console.log(rest2);
+// console.log(rest2);
+
+//////////////////
 
 //Destructuring objetcs
 
@@ -241,3 +244,97 @@ console.log(p, q, r); // 8 9 1
 */
 
 ///destructuring object
+
+// Coding Challenge #1
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const [player1, player2] = game.players;
+
+console.log(player1);
+console.log(player2);
+
+//2
+const [gk, ...fieldPlayers] = player1;
+console.log(gk);
+console.log(fieldPlayers);
+
+//3
+const allplayers = [...player1, ...player2];
+console.log(allplayers);
+
+//4
+const player1Final = ['Thiago', 'Coutinho', 'Perisic', , ...player1];
+console.log(player1Final);
+
+//5
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+
+//6
+function printGoals(...players) {
+  console.log(`${players.length} goals were scored `);
+}
+printGoals(...game.scored);
+
+//7
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
